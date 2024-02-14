f4c38df605a099804c0565105b424f26f01cbed8
unknown <gustavolscmidt13@gmail.com>
2024-02-13 23:23:12 -0300

    second commit

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const livros = [
     {
         id: 1,
@@ -13,13 +15,29 @@ const livros = [
     }
 ]
 
+function buscaLivre(id) {
+    return livros.findIndex(livro => {
+        return livro.id === number(id);
+    })
+}
 
-app.get("/", (req,res) => {
+
+
+app.get("/", (req, res) => {
     res.status(200).send("curso de node js");
 })
 
 app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
+
+app.get("/livros/:id", (req, res) => {
+
+})
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("livro cadastrado com sucesso")
+})
 
 export default app;
